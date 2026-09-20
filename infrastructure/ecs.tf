a5a4cc59f08a9a0bd5acdaddeec95ebac7372284
VinGeo1 <123502241+VinGeo1@users.noreply.github.com>
2026-09-20 18:55:32 +0530

    add

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -33,15 +33,20 @@ resource "aws_security_group" "ecs_instance" {
 
 resource "aws_launch_template" "ecs" {
   name_prefix   = "ecs-template"
-  image_id      = "ami-0c7217cdde317cfec"
+  image_id      = "ami-04fcaec208e9dd0f1"
   instance_type = "t3.micro"
+
   iam_instance_profile {
     name = aws_iam_instance_profile.ecs.name
   }
+
   vpc_security_group_ids = [aws_security_group.ecs_instance.id]
+
   user_data = base64encode(<<-EOT
     #!/bin/bash
-    echo ECS_CLUSTER=ecommerce-cluster >> /etc/ecs/ecs.config
+    echo "ECS_CLUSTER=ecommerce-cluster" >> /etc/ecs/ecs.config
+    echo "ECS_ENABLE_CONTAINER_METADATA=true" >> /etc/ecs/ecs.config
+    systemctl enable --now ecs
   EOT
   )
 }
@@ -64,14 +69,22 @@ data "aws_iam_policy_document" "ec2_assume" {
   }
 }
 
+resource "aws_iam_role_policy_attachment" "ecs_ec2_service_role" {
+  role       = aws_iam_role.ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
 resource "aws_autoscaling_group" "ecs" {
   vpc_zone_identifier = aws_subnet.private[*].id
+
   launch_template {
     id      = aws_launch_template.ecs.id
     version = "$Latest"
   }
+
   min_size = 1
   max_size = 3
+
   target_group_arns = [
     aws_lb_target_group.backend.arn,
     aws_lb_target_group.frontend.arn
@@ -103,17 +116,20 @@ resource "aws_ecs_task_definition" "backend" {
   requires_compatibilities = ["EC2"]
   cpu                      = "256"
   memory                   = "512"
+
   container_definitions = jsonencode([{
     name      = "backend"
     image     = var.backend_image
     cpu       = 256
     memory    = 256
     essential = true
+
     portMappings = [{
       containerPort = 8080
       hostPort      = 8080
       protocol      = "tcp"
     }]
+
     logConfiguration = {
       logDriver = "awslogs"
       options = {
@@ -130,11 +146,14 @@ resource "aws_ecs_service" "backend" {
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.backend.arn
   desired_count   = 1
+
   load_balancer {
     target_group_arn = aws_lb_target_group.backend.arn
     container_name   = "backend"
     container_port   = 8080
   }
+
+  depends_on = [aws_lb_listener.http]
 }
 
 resource "aws_ecs_task_definition" "frontend" {
@@ -143,17 +162,20 @@ resource "aws_ecs_task_definition" "frontend" {
   requires_compatibilities = ["EC2"]
   cpu                      = "256"
   memory                   = "512"
+
   container_definitions = jsonencode([{
     name      = "frontend"
     image     = var.frontend_image
     cpu       = 256
     memory    = 256
     essential = true
+
     portMappings = [{
       containerPort = 80
       hostPort      = 80
       protocol      = "tcp"
     }]
+
     logConfiguration = {
       logDriver = "awslogs"
       options = {
@@ -170,11 +192,14 @@ resource "aws_ecs_service" "frontend" {
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.frontend.arn
   desired_count   = 1
+
   load_balancer {
     target_group_arn = aws_lb_target_group.frontend.arn
     container_name   = "frontend"
     container_port   = 80
   }
+
+  depends_on = [aws_lb_listener.http]
 }
 
 resource "aws_appautoscaling_target" "ecs" {
@@ -191,6 +216,7 @@ resource "aws_appautoscaling_policy" "cpu" {
   resource_id        = aws_appautoscaling_target.ecs.resource_id
   scalable_dimension = aws_appautoscaling_target.ecs.scalable_dimension
   service_namespace  = aws_appautoscaling_target.ecs.service_namespace
+
   target_tracking_scaling_policy_configuration {
     target_value = 75
     predefined_metric_specification {
